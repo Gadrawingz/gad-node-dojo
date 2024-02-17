a78707f7e6726dfdea3f298d38d3445606fedf26
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2024-02-17 22:32:31 +0200

    Commit on os and path modules of nodejs

---
+++ node1/module/os-module.js
@@ -0,0 +1,10 @@
+// Use of os module
+// Built-in node module used to get info+ about the computer's OS.
+const os = require('os');
+console.log(os.arch()); // x64
+console.log(os.platform()); // linux
+console.log(os.networkInterfaces()); //
+console.log(os.cpus()) // 
+console.log(os.totalmem()) // Total memory
+console.log(os.freemem()) // Free memory
+
---
+++ node1/module/path-module.js
@@ -0,0 +1,10 @@
+// Use of Path module
+const path = require('path')
+// 1. Extension
+let ext = path.extname('/home/gadrone/Gadrawingz/Gadevelop/javascriptin4/node-practice/gad-node-dojo/node1/module/test-file1.txt')
+console.log(ext); // .ext.
+
+// 2. Base Name
+let baseName = path.extname('/home/gadrone/Gadrawingz/Gadevelop/javascriptin4/node-practice/gad-node-dojo/node1/module/test-file1.txt')
+console.log(__filename) // shows file name
+console.log(__dirname) // show directory name
